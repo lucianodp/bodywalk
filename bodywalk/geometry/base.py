3e20f2a10bd00a345f2b12258103284c822269d8
lucianodp <lucianopalma@protonmail.com>
2021-12-11 21:19:31 +0100

    Improved ConvexBody documentation

--- bodywalk/geometry/base.py
+++ bodywalk/geometry/base.py
@@ -5,14 +5,20 @@ import numpy as np
 
 
 class ConvexBody(ABC):
-    """Abstract class representing a general convex body in the euclidean space, R^n.
-    You can define your own convex bodies by directly subclassing ConvexBody and implementing
-    the required abstract methods.
+    """Interface representing a general convex body in the euclidean space, exposing the
+    necessary functionality for running all the sampling algorithms supported by this
+    library (Ball Walk, Hit-and-Run, ...).
+
+    We also note that each sampling algorithm may only require a small subset of methods
+    to be implemented; for example, to run the Ball Walk sampler it is enough to implement
+    the is_inside() method. Please refer to the documentation of each sampling function
+    for a list of all methods it needs implemented.
     """
 
-    @abstractmethod
     def is_inside(self, x: np.ndarray) -> bool:
-        """Check if a data point is inside this convex body (self)
+        """Check if a data point is inside this convex body (self).
+
+        This method must be implemented to run the Ball Walk sampling algorithm.
 
         Parameters
         ----------
@@ -24,14 +30,16 @@ class ConvexBody(ABC):
         bool
             True if x is inside this convex body (self); False otherwise
         """
+        raise NotImplementedError("'is_inside' method must be implemented to run the Ball Walk sampler.")
 
-    @abstractmethod
     def compute_intersection_extremes(self, x: np.ndarray, v: np.ndarray) -> Tuple[float, float]:
         """Given an interior point "x" and a direction vector "v", this method computes the
         intersection between the straight line {x + t * v} and this convex body (self). More precisely,
         this method returns a pair of numbers (L, U) satisfying:
 
                 "x + t * v" is inside this convex body (self)  <=>  L <= t <= U.
+
+        This method must be implemented to run the Hit-and-Run sampling algorithm.
 
         Parameters
         ----------
@@ -45,8 +53,8 @@ class ConvexBody(ABC):
         Tuple[float, float]
             The pair (L, U) corresponding to the extremes of the line segment. L must be smaller than U.
         """
+        raise NotImplementedError("'compute_intersection_extremes' method must be implemented to run the Hit-and-Run sampler.")
 
-    @abstractmethod
     def compute_boundary_reflection(self, x: np.ndarray, v: np.ndarray) -> Tuple[np.ndarray, float]:
         """Given an interior point "x" and a direction vector "v", this method computes where the ray
         {x + tv, t > 0} hits the boundary of this convex body. More precisely, this method returns two
@@ -56,6 +64,8 @@ class ConvexBody(ABC):
                                 and pointing inwards to the convex body.
 
             2) Distance: the distance between "x" and the hit point
+
+        This method must be implemented to run the Billiard Walk sampling algorithm.
 
         Parameters
         ----------
@@ -69,3 +79,4 @@ class ConvexBody(ABC):
         Tuple[np.ndarray, float]
             The internal normal vector at the hit point, and the distance to the hit point
         """
+        raise NotImplementedError("'compute_boundary_reflection' method must be implemented to run the Billiard Walk sampler.")
